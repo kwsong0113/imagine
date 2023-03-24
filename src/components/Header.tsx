79dfe18a6c05c01868a4b0645d0488bd9333cb2f
Kiwhan Song <kwsong0113@gmail.com>
2023-03-24 16:33:22 +0900

    feat: test animation for backButton

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
-import { TouchableOpacity } from 'react-native';
+import { TouchableWithoutFeedback } from 'react-native';
 import { VStack, HStack, Center } from 'native-base';
 import { IonIcon } from './IonIcon';
 import { Typography } from './Typography';
 import { useNavigation } from '@react-navigation/native';
+import Animated, {
+  useSharedValue,
+  withTiming,
+  useAnimatedStyle,
+  interpolate,
+  Extrapolate,
+} from 'react-native-reanimated';
 
 interface HeaderProps {
   variant?: 'left' | 'center';
@@ -19,14 +26,40 @@ export const Header = ({
   description,
 }: HeaderProps) => {
   const navigation = useNavigation();
+  const pressing = useSharedValue(0);
+
+  const outlineStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          scale: interpolate(
+            pressing.value,
+            [0, 1],
+            [1, 0.7],
+            Extrapolate.CLAMP,
+          ),
+        },
+      ],
+    };
+  });
 
   return (
-    <VStack space="3" bg="teal.100">
+    <VStack space="3">
       <HStack space="2.5" alignItems="center">
         {hasBackButton && (
-          <TouchableOpacity activeOpacity={0.4} onPress={navigation.goBack}>
-            <IonIcon name="chevron-back-circle" color="gray.500" size="30" />
-          </TouchableOpacity>
+          <TouchableWithoutFeedback
+            onPressIn={() => {
+              pressing.value = withTiming(1);
+            }}
+            onPressOut={() => {
+              pressing.value = withTiming(0);
+            }}
+            onPress={navigation.goBack}
+          >
+            <Animated.View style={outlineStyle}>
+              <IonIcon name="chevron-back-circle" color="gray.500" size="30" />
+            </Animated.View>
+          </TouchableWithoutFeedback>
         )}
         {variant === 'left' ? (
           <Typography variant="title">{title}</Typography>
