31f9ec0f8cb12de10962198a7847e0f614430697
Kiwhan Song <kwsong0113@gmail.com>
2023-03-24 16:34:20 +0900

    feat: add simple colormode switch to setting

--- src/screens/Setting.tsx
+++ src/screens/Setting.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Center, Switch } from 'native-base';
+import { useColorMode } from '../hooks';
+import { useAppDispatch } from '../hooks';
+import { settingActions } from '../store/slices';
 
 export const Setting = () => {
+  const colorMode = useColorMode();
+  const dispatch = useAppDispatch();
+
   return (
-    <View>
-      <Text>Setting</Text>
-    </View>
+    <Center flex={1} bg="gray.100" safeArea>
+      <Switch
+        isChecked={colorMode === 'light'}
+        onTrackColor="orange.800"
+        onThumbColor="gray.100"
+        offTrackColor="gray.300"
+        offThumbColor="gray.100"
+        onToggle={() => {
+          dispatch(
+            settingActions.changeTheme(
+              colorMode === 'light' ? 'dark' : 'light',
+            ),
+          );
+        }}
+      />
+    </Center>
   );
 };
