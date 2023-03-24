dcc721b6521425fd6e4a9149789294a283e90ed4
Kiwhan Song <kwsong0113@gmail.com>
2023-03-24 23:10:50 +0900

    feat: add help AnimatedIconButton to custom home screen

--- src/screens/Custom/Custom.tsx
+++ src/screens/Custom/Custom.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
-import { Pressable } from 'native-base';
-import { Header, IonIcon, ScreenContainer } from '../../components';
+import { HStack, Pressable } from 'native-base';
+import {
+  AnimatedIconButton,
+  Header,
+  IonIcon,
+  ScreenContainer,
+} from '../../components';
 import { CustomStackParamList } from '../../navigation';
 import { StackScreenProps } from '@react-navigation/stack';
 
@@ -9,11 +14,14 @@ type Props = StackScreenProps<CustomStackParamList, 'Home'>;
 export const Custom = ({ navigation }: Props) => {
   return (
     <ScreenContainer>
-      <Header
-        hasBackButton={false}
-        title="커스텀 제스처"
-        description="원하는 액션을 커스텀 제스처로 실행해요"
-      />
+      <HStack justifyContent="space-between">
+        <Header
+          hasBackButton={false}
+          title="커스텀 제스처"
+          description="원하는 액션을 커스텀 제스처로 실행해요"
+        />
+        <AnimatedIconButton name="hand-left" color="orange.700" size={26} />
+      </HStack>
       <Pressable onPress={() => navigation.navigate('AppList')}>
         <IonIcon name="chevron-forward-outline" size={30} />
       </Pressable>
