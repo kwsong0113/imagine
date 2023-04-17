368a34dca2804cc3358a73da9dc446e899cfd96f
Kiwhan Song <kwsong0113@gmail.com>
2023-04-17 15:19:14 +0900

    fix: round number of actions per day

--- src/screens/Statistics/Statistics.tsx
+++ src/screens/Statistics/Statistics.tsx
@@ -75,7 +75,7 @@ export const Statistics = ({ navigation }: Props) => {
           />
           <CountInfo
             flex={1}
-            count={Math.round(numActionsPerDay * 10) / 10}
+            count={Math.round(numActionsPerDay)}
             description="하루 평균 액션 실행 횟수"
           />
         </HStack>
