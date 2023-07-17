cbcaa0ba573ef5331a328297ba80197739fd4fde
Kiwhan Song <kwsong0113@gmail.com>
2023-07-17 15:53:45 +0900

    chore: configure xcode cloud workflow (#34)
    * fix: xcode cloud build errors
    
    * fix: edit versioning in custom build script
    
    * chore: clean up build script

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
-
-# Install CocoaPods using Homebrew.
+export HOMEBREW_NO_INSTALL_CLEANUP=TRUE
 brew install cocoapods
+# have to add node yourself
+brew install node@20
+# link it to the path
+brew link node@20
 
+brew install yarn
+yarn set version 3.5.0
 
 # Install dependencies you manage with CocoaPods.
+yarn
 pod install
