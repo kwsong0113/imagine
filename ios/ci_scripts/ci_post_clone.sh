1f5279fff1574e2b057ea523fef84229109602e2
Kiwhan Song <kwsong0113@gmail.com>
2023-07-17 14:19:29 +0900

    fix: xcode cloud build errors

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
 
+export HOMEBREW_NO_INSTALL_CLEANUP=TRUE
+brew install cocoapods@1.12.1
+# have to add node yourself
+brew install node@20.0.0
+# link it to the path
+brew link node@20.0.0
 
-# Install CocoaPods using Homebrew.
-brew install cocoapods
+brew install yarn
+yarn set version 3.5.0
 
+yarn -v
+node -v
+pod --version
 
 # Install dependencies you manage with CocoaPods.
+yarn
 pod install
